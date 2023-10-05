49f8ff6b93f6e6a71bea28d2d239a1c655d0bea7
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-10-05 18:16:43 +0530

    added message emit to all users and log-on messages

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get('/',(req,res) => {
     res.sendFile(__dirname + '/templates/index.html')
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
 server.listen(3000,() => {
     console.log('Listening on port 3000');
 });
