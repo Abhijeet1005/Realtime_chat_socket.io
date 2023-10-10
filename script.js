d224046a731908a78ea22280df0aee6631fe8e93
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-10-10 17:48:29 +0530

    added login functionality without database for now

--- script.js
+++ script.js
@@ -5,20 +5,43 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+const users = {"Abhijeet": "password"}; // This works as a temporary solution for storing registered users and will replace it with a DB with hashed passwords later on
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine","ejs");
 app.use(express.static('./public'));
 
 app.get('/',(req,res) => {
     res.render("auth");
 });
+
+
 app.get('/login',(req,res) => {
-    res.render("Login");
+    res.render("Login",{errorMessage: ''});
 });
+
+
+app.post('/login',(req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if(users[username] == password){
+    res.redirect("chatpage");
+  }
+  else{
+    res.render("Login", { errorMessage: "Authentication failed. Please try again." });
+  }
+  
+});
+
+
+app.get('/chatpage',(req,res) => {
+    res.render("chatpage");
+});
+
+
 app.get('/signup',(req,res) => {
     res.render("signup");
-});
-app.post('/',(req,res) => {
-  res.sendStatus(500);
 });
 
 io.on('connection', (socket) => {
