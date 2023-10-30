f4a51d3f432d19a707435f31634eb8132d0574a0
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2023-10-30 15:30:32 +0530

    minor changes for atlas

--- utils/model.js
+++ utils/model.js
@@ -3,10 +3,7 @@ require('dotenv').config({ path: '../' });
 
 const dbConnect = async function(){
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB connected")
       
       } catch (err) {
